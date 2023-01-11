606bd6967c67e37de5c490117602c5cbc98b0af6
vinoop vennekode <vinoopvennekode@gmail.com>
2023-01-11 21:58:15 +0530

    listed movies in Action and Orginals,added youtube window to watch trailer of themovies

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import NavBar from './Components/navBar/NavBar';
 import Banner from './Components/Banner/banner';
 import RowPost from './Components/RowPost/RowPost';
+import {Action,original} from './url'
 function App() {
   return (
     <div className="App">
    <NavBar/>
    <Banner/>
-   <RowPost/>
+   <RowPost url={original} title='Netflix Originals'/>
+   <RowPost url={Action} title='Action' isSmall/>
     </div>
   );
 }
