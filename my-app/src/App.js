9680cffcee79e6ddab4a74f8e48ed3de514c54ad
vinoop vennekode <vinoopvennekode@gmail.com>
2023-01-11 17:52:42 +0530

    banner competed,banner have an image,Movie title and description

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/navBar/NavBar';
+import Banner from './Components/Banner/banner';
 
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+   <NavBar/>
+   <Banner/>
     </div>
   );
 }
