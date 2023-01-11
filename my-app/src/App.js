d2c6a27a67a2164314fe19637cf9e647f8c5fe2a
vinoop vennekode <vinoopvennekode@gmail.com>
2023-01-11 18:21:09 +0530

    movie posters completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import NavBar from './Components/navBar/NavBar';
 import Banner from './Components/Banner/banner';
-
+import RowPost from './Components/RowPost/RowPost';
 function App() {
   return (
     <div className="App">
    <NavBar/>
    <Banner/>
+   <RowPost/>
     </div>
   );
 }
